a0008aaaf483cdc2cf4bb79d7f9e9bcaad2c4b6a
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2024-06-12 16:32:34 -0700

    Create todo and checklist factory functions
    Add shared functions by composition.
    
    Export the 2 factory functions for other modules.

---
+++ src/todo.js
@@ -0,0 +1,42 @@
+// module for todo and checklist//
+
+function createTodo (title,description,dueDate,priority,...checklistArray){
+    isComplete = false;
+
+    const incrementPriority = function(){
+        priority++;
+    }
+
+    const decrementPriority = function(){
+        priority--;
+    }
+
+    return {
+        title,
+        description,
+        dueDate,
+        priority,
+        isComplete,
+        checklistArray,
+        ...completeTogglable(),
+        incrementPriority,
+        decrementPriority,
+    }
+}
+
+function createChecklist(description) {
+    isComplete = false;
+
+    return {
+        isComplete,
+        ...completeTogglable(),
+    }
+}
+
+function completeTogglable() {
+    return {
+        toggleComplete: () => (this.isComplete = !this.isComplete),
+    }
+}
+
+export {createTodo, createChecklist};
