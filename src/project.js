43eeb1a99092fc55e022ed5c51197c8d1b5bd0ab
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2024-06-13 11:48:04 -0700

    Allow adding or removing Todos from project

--- src/project.js
+++ src/project.js
@@ -3,19 +3,54 @@
 function createProject (name,...todoArray){
 
     let name = name;
-    
-    function getName(){
+    let isComplete = false;
+    let todoArray = todoArray;
+
+    const getName = function(){
         return name;
     }
     
-    function setName(newName){
+    const setName = function(newName){
         name = newName;
     }
 
+    const getTodoArray = function(){
+        return todoArray;
+    }
+
+    const addTodo = function(newTodo){
+        todoArray.push(newTodo);
+    }
+
+    const removeTodo = function(todoToRemove){
+        todoArray.pop(todoToRemove);
+    }
+
+    // sort by priority only with 3 being highest, 1 being lowest
+    const sortTodos = function(){
+        
+    }
+
+    const updateComplete = function (boolean){
+        if (boolean === undefined){
+            isComplete = isAllTodosComplete();
+        } else {
+            isComplete = boolean;
+        }
+    }
+
+    const isAllTodosComplete = function(){
+        for (todo of todoArray){
+            if (todo.isComplete === false){
+                return false;
+            }
+        }
+        return true;
+    }
+
     return {
-        name,
-        ...todoArray,
         getName,
         setName,
+        updateComplete,
     }
 }
