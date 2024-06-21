6c6b9908fb52a24ad2b651cfa7ed25f284510639
Jiwon Jeong <jiwon.j.jeong@gmail.com>
2024-06-21 00:02:44 -0700

    Make set and get functions for localStorage

---
+++ src/storage.js
@@ -0,0 +1,90 @@
+// this module is for working with localStorage to store data //
+import {pageManager} from "./index.js";
+
+const dataManager = function () {
+    let projects;
+    let todos;
+
+    const storageAvailable = function (type) {
+        let storage;
+        try {
+            storage = window[type];
+            const x = "__storage_test__";
+            storage.setItem(x, x);
+            storage.removeItem(x);
+            return true;
+        } catch (e) {
+            return (
+                e instanceof DOMException &&
+                // everything except Firefox
+                (e.code === 22 ||
+                    // Firefox
+                    e.code === 1014 ||
+                    // test name field too, because code might not be present
+                    // everything except Firefox
+                    e.name === "QuotaExceededError" ||
+                    // Firefox
+                    e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+                // acknowledge QuotaExceededError only if there's something already stored
+                storage &&
+                storage.length !== 0
+            );
+        }
+    }
+
+    const checkStorageAvailable = function(){
+        return Boolean(storageAvailable("localStorage"));
+    }
+
+    const getProjectsAndTodosFromStorage = function(){
+        let i = 0;
+        let projectFromStorage = localStorage.getItem(`project${i}`);
+        while (projectFromStorage !== null){
+            projects.push(JSON.parse(projectFromStorage));
+        }
+        i = 0;
+        let todoFromStorage = localStorage.getItem(`todo${i}`);
+        while (todoFromStorage !== null){
+            todos.push(JSON.parse(todoFromStorage));
+        }
+    }
+
+    const reassignMethodsToObjectsFromStorage = function(){
+        for (let project of projects){
+
+        }
+        for (let todo of todos){
+
+        }
+    }
+
+    const setAllProjectsAndTodos = function(){
+        const projects = pageManager.getProjectsAndNakedTodos().projects;
+        const todos = pageManager.getProjectsAndNakedTodos().todosWithoutProject;
+        for (let project of projects){
+            setProjectOrTodo(project);
+        }
+        for (let todo of todos){
+            setProjectOrTodo(todo);
+        }
+    }
+
+    const setProjectOrTodo = function(object){
+        const projects = pageManager.getProjectsAndNakedTodos().projects;
+        const todos = pageManager.getProjectsAndNakedTodos().todosWithoutProject;
+        const projectIndex = projects.indexOf(object);
+        const todoIndex = todos.indexOf(object);
+        if (projectIndex !== -1){
+            localStorage.setItem(`project${projectIndex}`, JSON.stringify(object));
+        } else {
+            localStorage.setItem(`todo${todoIndex}`, JSON.stringify(object));
+        }
+    }
+
+    return {
+        setAllProjectsAndTodos, setProjectOrTodo,
+
+    }
+
+
+}();
